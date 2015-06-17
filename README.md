3cb42a11b40527a9adfee6abaab7e00a2b3f6900
Nicholas Drone <nd26434@ex.kroger.com>
2015-06-17 17:35:14 -0400

    initial commit of working project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# datatable-thymeleaf-spring-boot-starter
+datatables-thymeleaf-spring-boot-starter
+========================================
+
 The simpliest application for show how to setup Dandelion-datatables when using Thymeleaf and Spring Boot
+
+## Technology Stack
+
+ - Java 1.8
+ - Spring Boot 1.2.4.RELEASE
+ - Thymeleaf 2.1.4.RELEASE
+ - Dandelion-Datatables 0.10.1
+ 
+## Running this sample
+ Using __Maven__:
+    `mvn clean package`
+    `java -jar target/datatables-thymeleaf-spring-boot-starter-0.0.1-SNAPSHOT.jar`
+    
+ You then can access the sample here: [http://localhost:8080/table] (http://localhost:8080/table)
---
+++ src/main/java/org/test/model/Person.java
@@ -0,0 +1,41 @@
+package org.test.model;
+
+/**
+ * Created by nd26434 on 6/17/2015.
+ */
+public class Person
+{
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+}
